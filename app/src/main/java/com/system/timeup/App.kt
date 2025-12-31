5ef6824822e81fb5dab9bdf5169a9c8740def202
1 <111@163.com>
2025-12-31 20:24:37 +0800

    1

--- app/src/main/java/com/system/timeup/App.kt
+++ app/src/main/java/com/system/timeup/App.kt
@@ -6,11 +6,12 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        // 原来的保活
-        AlarmKick.ensure(this, seconds = 180, reason = "进程启动软补排")
-        WorkKick.ensurePeriodic(this)
 
         // 新增：启动 C++ 后台线程（非阻塞）
-        NativeBackend.start()
+        //NativeBackend.start()
+
+        // 原来的保活逻辑
+        AlarmKick.ensure(this, seconds = 180, reason = "进程启动软补排")
+        WorkKick.ensurePeriodic(this)
     }
 }
